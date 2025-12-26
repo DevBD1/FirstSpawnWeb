e91463989a8019e101a86097ec42a966f6625e4b
DevBD1 <burakdorman@gmail.com>
2025-12-26 17:16:02 +0300

    chore: remove experimental allowedDevOrigins from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ["172.19.160.1"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
